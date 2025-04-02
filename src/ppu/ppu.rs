9cef6c55e3468f80222e1369f37abdc189bbff50
Chris <cfreeman10538@gmail.com>
2025-04-01 22:23:43 -0400

    rework PPU to do cycle-accurate sprite fetches, general PPU refactoring

--- src/ppu/ppu.rs
+++ src/ppu/ppu.rs
@@ -23,8 +23,10 @@ const PPU_SCROLL: u16 = 5;
 const PPU_ADDR: u16 = 6;
 const PPU_DATA: u16 = 7;
 const PALETTE_RAM_SIZE: usize = 32;
-const PRE_FETCH_START: u32 = 321;
-const PRE_FETCH_END: u32 = 336;
+const BG_PRE_FETCH_START: u32 = 321;
+const BG_PRE_FETCH_END: u32 = 336;
+const SPRITE_FETCH_START: u32 = 258;
+const SPRITE_FETCH_END: u32 = 320;
 const ATTR_TABLE_OFFSET: u32 = 0x23C0;
 const PALETTE_START: u16 = 0x3F00;
 pub const OAM_SIZE: usize = 256;
@@ -34,8 +36,9 @@ const SPRITES_PER_ROW: usize = 8;
 struct SpriteInfo {
     x_pos: u8,
     y_pos: u8,
-    pattern_0: u8,
-    pattern_1: u8,
+    fine_y: u8,
+    pattern_table: u8,
+    tile: u8,
     attr: SpriteAttr,
     sprite_0: bool,
 }
@@ -45,9 +48,10 @@ impl Default for SpriteInfo {
         Self {
             x_pos: 0xFF,
             y_pos: 0xFF,
-            pattern_0: 0xFF,
-            pattern_1: 0xFF,
-            attr: SpriteAttr { data: 0xff },
+            fine_y: 0xFF,
+            pattern_table: 0xFF,
+            tile: 0xFF,
+            attr: SpriteAttr { data: 0xFF },
             sprite_0: false,
         }
     }
@@ -66,14 +70,16 @@ pub struct Ppu {
     nmi: bool,
     bg_shift_reg_0: u16,
     bg_shift_reg_1: u16,
-    attr_shift_reg_0: u16,
-    attr_shift_reg_1: u16,
-    tile_num: u8,
-    attr_byte: u8,
-    bg_byte_0: u8,
-    bg_byte_1: u8,
+    bg_attr_shift_reg_0: u16,
+    bg_attr_shift_reg_1: u16,
+    bg_tile_num: u8,
+    bg_attr: u8,
+    bg_pattern_0: u8,
+    bg_pattern_1: u8,
     oam: [u8; OAM_SIZE],
-    sprites: Vec<SpriteInfo>,
+    sprite_infos: [SpriteInfo; SPRITES_PER_ROW],
+    sprite_patterns_0: [u8; SPRITES_PER_ROW],
+    sprite_patterns_1: [u8; SPRITES_PER_ROW],
     oam_addr: u8,
     cycles: u32,
     frame_cycle: u32,
@@ -94,14 +100,16 @@ impl Default for Ppu {
             nmi: Default::default(),
             bg_shift_reg_0: Default::default(),
             bg_shift_reg_1: Default::default(),
-            attr_shift_reg_0: Default::default(),
-            attr_shift_reg_1: Default::default(),
-            tile_num: Default::default(),
-            attr_byte: Default::default(),
-            bg_byte_0: Default::default(),
-            bg_byte_1: Default::default(),
+            bg_attr_shift_reg_0: Default::default(),
+            bg_attr_shift_reg_1: Default::default(),
+            bg_tile_num: Default::default(),
+            bg_attr: Default::default(),
+            bg_pattern_0: Default::default(),
+            bg_pattern_1: Default::default(),
             oam: [0xFF; OAM_SIZE],
-            sprites: vec![],
+            sprite_infos: [SpriteInfo::default(); SPRITES_PER_ROW],
+            sprite_patterns_0: [0; SPRITES_PER_ROW],
+            sprite_patterns_1: [0; SPRITES_PER_ROW],
             oam_addr: Default::default(),
             cycles: Default::default(),
             frame_cycle: Default::default(),
@@ -120,7 +128,7 @@ impl Ppu {
 
         if row < DISPLAY_HEIGHT || row == NUM_ROWS - 1 {
             if (col >= 1 && col <= DISPLAY_WIDTH)
-                || (col >= PRE_FETCH_START && col <= PRE_FETCH_END)
+                || (col >= BG_PRE_FETCH_START && col <= BG_PRE_FETCH_END)
             {
                 if self.bg_enabled() {
                     self.update_shift_regs();
@@ -152,12 +160,21 @@ impl Ppu {
                 self.v.set_coarse_x(self.t.coarse_x());
                 self.v.set_nx(self.t.nx());
             }
+
+            if col >= SPRITE_FETCH_START && col <= SPRITE_FETCH_END && self.rendering_enabled() {
+                let sprite_idx = ((col - SPRITE_FETCH_START) / 8) as usize;
+                match (col - SPRITE_FETCH_START) % 8 {
+                    4 => self.fetch_sprite_pattern(bus, 0, sprite_idx),
+                    6 => self.fetch_sprite_pattern(bus, 1, sprite_idx),
+                    _ => (),
+                }
+            }
         }
 
         if col == DISPLAY_WIDTH + 1 {
-            self.sprites.clear();
+            self.sprite_infos.fill(SpriteInfo::default());
 
-            if row < DISPLAY_HEIGHT || row == NUM_ROWS - 1 {
+            if row < DISPLAY_HEIGHT {
                 self.sprite_eval(row, bus);
             }
         }
@@ -269,7 +286,7 @@ impl Ppu {
     }
 
     fn fetch_bg_tile_num(&mut self, bus: &mut impl PpuBus) {
-        self.tile_num = bus.ppu_read(NAMETABLE_0_START | self.v.nt_addr());
+        self.bg_tile_num = bus.ppu_read(NAMETABLE_0_START | self.v.nt_addr());
     }
 
     fn fetch_bg_attr(&mut self, bus: &mut impl PpuBus) {
@@ -277,49 +294,49 @@ impl Ppu {
         attr_addr.set_tile_group_x(self.v.coarse_x() / 4);
         attr_addr.set_tile_group_y(self.v.coarse_y() / 4);
         attr_addr.set_n(self.v.n());
-        self.attr_byte = bus.ppu_read(attr_addr.data());
+        self.bg_attr = bus.ppu_read(attr_addr.data());
 
         let tile_group_right = (self.v.coarse_x() % 4 > 1) as u8;
         let tile_group_bottom = (self.v.coarse_y() % 4 > 1) as u8;
         let shift_amt = ((tile_group_bottom << 1) | tile_group_right) * 2;
-        self.attr_byte >>= shift_amt;
+        self.bg_attr >>= shift_amt;
     }
 
     fn fetch_bg_pattern(&mut self, bus: &mut impl PpuBus, plane: u8) {
         let mut pattern_addr = PatternAddr::default();
         pattern_addr.set_fine_y(self.v.fine_y());
         pattern_addr.set_p(plane as u16);
-        pattern_addr.set_tile(self.tile_num as u16);
+        pattern_addr.set_tile(self.bg_tile_num as u16);
         pattern_addr.set_h(self.ctrl.b() as u16);
 
         if plane & 0x1 == 0 {
-            self.bg_byte_0 = bus.ppu_read(pattern_addr.data);
+            self.bg_pattern_0 = bus.ppu_read(pattern_addr.data);
         } else {
-            self.bg_byte_1 = bus.ppu_read(pattern_addr.data);
+            self.bg_pattern_1 = bus.ppu_read(pattern_addr.data);
         }
     }
 
     fn update_shift_regs(&mut self) {
         self.bg_shift_reg_0 <<= 1;
         self.bg_shift_reg_1 <<= 1;
-        self.attr_shift_reg_0 <<= 1;
-        self.attr_shift_reg_1 <<= 1;
+        self.bg_attr_shift_reg_0 <<= 1;
+        self.bg_attr_shift_reg_1 <<= 1;
     }
 
     fn load_shift_regs(&mut self) {
-        load_shift_reg(&mut self.bg_shift_reg_0, self.bg_byte_0);
-        load_shift_reg(&mut self.bg_shift_reg_1, self.bg_byte_1);
+        load_shift_reg(&mut self.bg_shift_reg_0, self.bg_pattern_0);
+        load_shift_reg(&mut self.bg_shift_reg_1, self.bg_pattern_1);
         load_shift_reg(
-            &mut self.attr_shift_reg_0,
-            if field!(self.attr_byte, 0, 1) == 1 {
+            &mut self.bg_attr_shift_reg_0,
+            if field!(self.bg_attr, 0, 1) == 1 {
                 0xFF
             } else {
                 0x00
             },
         );
         load_shift_reg(
-            &mut self.attr_shift_reg_1,
-            if field!(self.attr_byte, 1, 1) == 1 {
+            &mut self.bg_attr_shift_reg_1,
+            if field!(self.bg_attr, 1, 1) == 1 {
                 0xFF
             } else {
                 0x00
@@ -366,6 +383,117 @@ impl Ppu {
         self.v.set_ny(ny);
     }
 
+    fn get_bg_pixel_info(&mut self) -> PaletteAddr {
+        let mut palette_addr = PaletteAddr::default();
+        palette_addr.set_p0(self.fine_x(self.bg_shift_reg_0));
+        palette_addr.set_p1(self.fine_x(self.bg_shift_reg_1));
+        palette_addr.set_a0(self.fine_x(self.bg_attr_shift_reg_0));
+        palette_addr.set_a1(self.fine_x(self.bg_attr_shift_reg_1));
+
+        palette_addr
+    }
+
+    fn sprite_eval(&mut self, row: u32, bus: &mut impl PpuBus) {
+        let mut oam_idx = 0;
+        let mut sprites_found = 0;
+
+        while oam_idx < OAM_SIZE {
+            let y = self.oam[oam_idx];
+
+            let mut fine_y = row - (y as u32);
+            let y_max = if self.sprites_8x16() { 15 } else { 7 };
+
+            if (0..=y_max).contains(&fine_y) {
+                if sprites_found < SPRITES_PER_ROW {
+                    let mut tile = if !self.sprites_8x16() {
+                        self.oam[oam_idx + 1]
+                    } else {
+                        self.oam[oam_idx + 1] & !0x1
+                    };
+
+                    let pattern_table = if !self.sprites_8x16() {
+                        self.ctrl.s()
+                    } else {
+                        self.oam[oam_idx + 1] & 0x1
+                    } as u16;
+
+                    let attr = SpriteAttr {
+                        data: self.oam[oam_idx + 2],
+                    };
+
+                    let x = self.oam[oam_idx + 3];
+
+                    if attr.flip_ver() == 1 {
+                        fine_y = y_max - fine_y;
+                    }
+                    if fine_y > 7 {
+                        tile += 1;
+                    }
+
+                    self.sprite_infos[sprites_found] = SpriteInfo {
+                        x_pos: x,
+                        y_pos: y,
+                        fine_y: fine_y as u8,
+                        pattern_table: pattern_table as u8,
+                        tile: tile,
+                        attr: attr,
+                        sprite_0: oam_idx == 0,
+                    };
+
+                    sprites_found += 1;
+                } else {
+                    self.status.set_o(1);
+                    break;
+                }
+            }
+
+            oam_idx += 4;
+        }
+    }
+
+    fn fetch_sprite_pattern(&mut self, bus: &mut impl PpuBus, plane: u16, sprite_idx: usize) {
+        let sprite_info = self.sprite_infos[sprite_idx];
+        let mut pattern_addr = PatternAddr::default();
+        pattern_addr.set_fine_y(sprite_info.fine_y as u16);
+        pattern_addr.set_p(plane);
+        pattern_addr.set_tile(sprite_info.tile as u16);
+        pattern_addr.set_h(sprite_info.pattern_table as u16);
+
+        if plane & 0x1 == 0 {
+            self.sprite_patterns_0[sprite_idx] = bus.ppu_read(pattern_addr.data);
+        } else {
+            self.sprite_patterns_1[sprite_idx] = bus.ppu_read(pattern_addr.data);
+        }
+    }
+
+    fn get_sprite_pixel_info(&mut self, x: u32) -> (SpriteInfo, PaletteAddr) {
+        for idx in 0..SPRITES_PER_ROW {
+            let sprite = self.sprite_infos[idx];
+            let mut fine_x = x - sprite.x_pos as u32;
+            if sprite.attr.flip_hor() == 1 {
+                fine_x = 7 - fine_x;
+            }
+
+            let pattern_0 = self.sprite_patterns_0[idx] >> (7 - fine_x);
+            let pattern_1 = self.sprite_patterns_1[idx] >> (7 - fine_x);
+
+            if (0..8).contains(&fine_x)
+                && ((pattern_0 | pattern_1) & 0x1 != 0)
+                && sprite.fine_y != 0xFF
+            {
+                let mut palette_addr = PaletteAddr::default();
+                palette_addr.set_p0(pattern_0 as u16);
+                palette_addr.set_p1(pattern_1 as u16);
+                palette_addr.set_a0(sprite.attr.palette0() as u16);
+                palette_addr.set_a1(sprite.attr.palette1() as u16);
+                palette_addr.set_s(1);
+                return (sprite, palette_addr);
+            }
+        }
+
+        (SpriteInfo::default(), PaletteAddr { data: 0 })
+    }
+
     fn draw_pixel(&mut self, x: u32, y: u32, frame: &mut [u8; FRAME_SIZE_BYTES]) {
         let frame_idx = ((y * DISPLAY_WIDTH + x) * 3) as usize;
         let bg_addr = self.get_bg_pixel_info();
@@ -403,108 +531,6 @@ impl Ppu {
             (true, false, false) => transparent_pixel,
             (true, false, true) => bg_pixel,
             (true, true, _) => sprite_pixel,
-        }
-    }
-
-    fn get_bg_pixel_info(&mut self) -> PaletteAddr {
-        let mut palette_addr = PaletteAddr::default();
-        palette_addr.set_p0(self.fine_x(self.bg_shift_reg_0));
-        palette_addr.set_p1(self.fine_x(self.bg_shift_reg_1));
-        palette_addr.set_a0(self.fine_x(self.attr_shift_reg_0));
-        palette_addr.set_a1(self.fine_x(self.attr_shift_reg_1));
-
-        palette_addr
-    }
-
-    fn get_sprite_pixel_info(&mut self, x: u32) -> (SpriteInfo, PaletteAddr) {
-        for sprite in self.sprites.iter() {
-            let mut fine_x = x - sprite.x_pos as u32;
-            if sprite.attr.flip_hor() == 1 {
-                fine_x = 7 - fine_x;
-            }
-
-            let pattern_0 = sprite.pattern_0 >> (7 - fine_x);
-            let pattern_1 = sprite.pattern_1 >> (7 - fine_x);
-
-            if (0..8).contains(&fine_x) && ((pattern_0 | pattern_1) & 0x1 != 0) {
-                let mut palette_addr = PaletteAddr::default();
-                palette_addr.set_p0(pattern_0 as u16);
-                palette_addr.set_p1(pattern_1 as u16);
-                palette_addr.set_a0(sprite.attr.palette0() as u16);
-                palette_addr.set_a1(sprite.attr.palette1() as u16);
-                palette_addr.set_s(1);
-                return (*sprite, palette_addr);
-            }
-        }
-
-        (SpriteInfo::default(), PaletteAddr { data: 0 })
-    }
-
-    fn sprite_eval(&mut self, row: u32, bus: &mut impl PpuBus) {
-        let mut oam_idx = 0;
-        let mut sprites_found = 0;
-
-        while oam_idx < OAM_SIZE {
-            let y = self.oam[oam_idx];
-
-            let mut tile = if !self.sprites_8x16() {
-                self.oam[oam_idx + 1]
-            } else {
-                self.oam[oam_idx + 1] & !0x1
-            };
-
-            let pattern_table = if !self.sprites_8x16() {
-                self.ctrl.s()
-            } else {
-                self.oam[oam_idx + 1] & 0x1
-            } as u16;
-
-            let attr = SpriteAttr {
-                data: self.oam[oam_idx + 2],
-            };
-
-            let x = self.oam[oam_idx + 3];
-
-            let mut fine_y = row - (y as u32);
-            let y_max = if self.sprites_8x16() { 15 } else { 7 };
-
-            if attr.flip_ver() == 1 {
-                fine_y = y_max - fine_y;
-            }
-            if fine_y > 7 {
-                tile += 1;
-            }
-
-            let mut fetch_pattern = |plane| -> u8 {
-                let mut pattern_addr = PatternAddr::default();
-                pattern_addr.set_fine_y(fine_y as u16);
-                pattern_addr.set_p(plane as u16);
-                pattern_addr.set_tile(tile as u16);
-                pattern_addr.set_h(pattern_table);
-
-                bus.ppu_read(pattern_addr.data)
-            };
-
-            let sprite_info = SpriteInfo {
-                x_pos: x,
-                y_pos: y,
-                pattern_0: fetch_pattern(0),
-                pattern_1: fetch_pattern(1),
-                attr: attr,
-                sprite_0: oam_idx == 0,
-            };
-
-            if (0..=y_max).contains(&fine_y) {
-                if sprites_found < SPRITES_PER_ROW {
-                    self.sprites.push(sprite_info);
-                    sprites_found += 1;
-                } else {
-                    self.status.set_o(1);
-                    break;
-                }
-            }
-
-            oam_idx += 4;
         }
     }
 
